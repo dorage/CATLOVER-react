4293ab4216d8f89545b01f90f6e00efba9920709
dorage <baloonflower554@gmail.com>
2019-09-16 18:00:10 +0900

    minor

--- src/Component/InfiniteScroll/InfiniteScrollPresenter.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import styled from 'styled-components';
-
-const InfiniteScrollPresenter = () => <></>;
-
-export default withRouter(InfiniteScrollPresenter);
--- src/Component/InfiniteScroll/index.js
+++
@@ -1,3 +0,0 @@
-import infinteScrollController from './InfiniteScrollController';
-
-export default infinteScrollController;
